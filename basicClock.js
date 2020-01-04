9f430d996b68cdef05ae63063b8c1dccddaabc97
shifted7 <59329478+shifted7@users.noreply.github.com>
2020-01-04 13:38:52 -0800

    Delete basicClock.js

--- basicClock.js
+++
@@ -1,7 +0,0 @@
-
-let currentTime = new Date();
-let currentHours = currentTime.getHours();
-let currentMinutes = currentTime.getMinutes();
-let currentSeconds = currentTime.getSeconds();
-let currentTimeString = currentHours + ':' + currentMinutes + ':' + currentSeconds;
-document.body.innerHTML = '<h1>' + currentTimeString + '</h1>';
