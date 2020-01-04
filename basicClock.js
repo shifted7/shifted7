329121665e6ccf419b1d9b3e272672b9676dd4a3
shifted7 <59329478+shifted7@users.noreply.github.com>
2020-01-04 00:04:09 -0800

    Update basicClock.js

--- basicClock.js
+++ basicClock.js
@@ -4,4 +4,4 @@ let currentHours = currentTime.getHours();
 let currentMinutes = currentTime.getMinutes();
 let currentSeconds = currentTime.getSeconds();
 let currentTimeString = currentHours + ':' + currentMinutes + ':' + currentSeconds;
-document.body.innerHTML = '<h1>' + currentTimeString + '</h1>'
+document.body.innerHTML = '<h1>' + currentTimeString + '</h1>';
